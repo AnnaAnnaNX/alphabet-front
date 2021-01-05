d6bb9e1ac02ab669ec1e8bc66be0c220adcf7a43
AnnaAnnaNX <anna.annanx@rambler.ru>
2021-01-05 09:53:08 +0300

    install apollo

--- src/main.js
+++ src/main.js
@@ -3,10 +3,43 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import VueApollo from "vue-apollo";
+
+import ApolloClient from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+Vue.use(VueApollo);
+
 Vue.config.productionTip = false;
+const getHeaders = () => {
+  const headers = {};
+  //  const token = window.localStorage.getItem('apollo-token');
+  //  if (token) {
+  //    headers.authorization = `Bearer ${token}`;
+  //  }
+   return headers;
+ };
+ // Create an http link:
+ const link = new HttpLink({
+   uri: 'https://alphabet.hasura.app/v1/graphql',
+   fetch,
+   headers: getHeaders()
+ });
+ const client = new ApolloClient({
+   link: link,
+   cache: new InMemoryCache({
+     addTypename: true
+   })
+ });
+
+const apolloProvider = new VueApollo({
+  defaultClient: client,
+})
 
 new Vue({
   router,
+  apolloProvider,
   store,
   render: h => h(App)
 }).$mount("#app");
